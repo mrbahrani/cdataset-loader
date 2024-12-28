8c89ae3ea4b6d86ea40990b68a580fedb6011cb1
abahrani <abahrani@starfishmedical.com>
2024-12-28 11:12:19 -0800

    project base

---
+++ include/cdataset_loader.h
@@ -0,0 +1,6 @@
+/*
+
+*/
+#include <stdio.h>
+
+int load_mnist(const char* directory, float* X_train, float* y_train, size_t train_size, float* X_test, float* y_test, size_t test_size);
---
+++ src/cdataset_loader.c
@@ -0,0 +1,5 @@
+#include "cdataset_loader.h"
+
+int load_mnist(const char* directory, float* X_train, float* y_train, size_t train_size, float* X_test, float* y_test, size_t test_size) {
+    return 0;
+}
